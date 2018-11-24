377488d7fd9ed07a08ea080a3db30d812db95798
Anders Larsen <anders@ikonhouse.com>
2018-11-24 13:27:17 +0400

    inclded inital design for list and letter anchors

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -15,8 +15,26 @@ class ListController extends AbstractController
     {
         $repository = $this->getDoctrine()->getRepository(Tennant::class);
 
+        //$tennantList = $repository->findAll();
+        $tennantList = $repository->findBy([], ['name' => 'ASC']);
+
+
+
+        $last = '';
+
+        foreach ($tennantList as $tennant) {
+            $current = strtolower($tennant->getName()[0]);
+            if ($last != $current) {
+                $letterArr[] = strtoupper($current);
+                //echo strtoupper($current) . "<br />";
+                $last = $current;
+            }
+            //echo "<a href=\"/artists/artist.php?user=".$row{'user'}."\">".$row{'name'}."</a><br />";
+        }
+
         return $this->render('list/index.html.twig', [
-            'tennants' => $repository->findAll()
+            'tennants' => $tennantList,
+            'letters' => $letterArr
 
         ]);
     }
